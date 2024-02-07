8f02d7b2e6b97809d24517e7d65416740a35798d
Jarek Frankiewicz <frantz12@wp.pl>
2024-02-07 18:41:13 +0100

    Add project files.

---
+++ Day4/Program.cs
@@ -0,0 +1,16 @@
+﻿string name = "Kuba";
+string sex = "men";
+int age = 10;
+
+if (sex == "women" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "men" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
